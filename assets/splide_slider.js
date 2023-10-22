22c53226ec9f7dfdb360522098658c539ed0cf4e
rdnvol <66828584+rdnvol@users.noreply.github.com>
2023-10-22 17:56:48 +0300

    main part of the first section was done

---
+++ assets/splide_slider.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var splide = new Splide('.splide', {
+    mediaQuery: 'min',
+    breakpoints: {
+      650: {
+        destroy: true,
+      },
+    },
+    classes: {
+      pagination: 'splide__pagination splide__pagination-custom ',
+      page: 'splide__pagination__page splide__pagination__page-custom',
+    },
+  });
+  splide.mount();
+});
